b2791c93c09a30d1b869569d83c98805652a1159
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2024-08-16 03:27:20 +0530

    comit

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ menu.onclick = () => {
   navbar.classList.toggle('active')
 }
 
-window.onscroll = () => {
-  menu.classList.remove('fa-times')
-  navbar.classList.toggle('active')
-}
+// window.onscroll = () => {
+//   menu.classList.remove('fa-times')
+//   navbar.classList.toggle('active')
+// }
 
 document.querySelector('#search-icon').onclick = () => {
   document.querySelector('#search-form').classList.toggle('active');
