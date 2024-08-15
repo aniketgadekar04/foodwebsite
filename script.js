cfc6c447a5f512d0eec8971d303bc3b546b948f8
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2024-08-15 12:48:22 +0530

    ADD FEATURE

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
+menu.onclick = () => {
+  menu.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
 }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times')
-    navbar.classList.toggle('active')
+window.onscroll = () => {
+  menu.classList.remove('fa-times')
+  navbar.classList.toggle('active')
 }
 
 document.querySelector('#search-icon').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active');
+  document.querySelector('#search-form').classList.toggle('active');
 }
 
 document.querySelector('#close').onclick = () => {
-    document.querySelector('#search-form').classList.remove('active');
+  document.querySelector('#search-form').classList.remove('active');
 }
 
 
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5500,
-      disableOnInteraction: false,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    640: {
+      slidesPerView: 2,
     },
-    loop:true,
-  });
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
